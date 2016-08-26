26f4e9b7660bda2b78422126f7b7edaa3eccfc8a
Cody Ogden <cody@codyogden.com>
2016-08-26 07:44:28 -0500

    Clean Up, Clean Up, Everybody Do Your Share

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
+// Let's preserve the namespace
 var employees = new Object();
-
+// Initialize the employee datastore
 employees.data = [];
+// Faux serializing the data
 employees.fields = ["firstNameInput","lastNameInput","employeeIdInput","jobTitleInput","salaryInput"];
+// Preserves the "No Employees" view
 employees.defaultMessage = document.getElementById( "defaultDisplay" );
-
-employees.addEmployee = function () { // Add a new employee
-
+// Add a new employee
+employees.addEmployee = function () {
 	// Ensure all form fields were filled out, else return false
 	for( var i = 0; i < this.fields.length; i++ ) {
 		var field = document.getElementById( this.fields[i] ).value;
@@ -25,34 +27,36 @@ employees.addEmployee = function () { // Add a new employee
 	}
 	// Reset the add employee form ( clears all fields )
 	this.resetForm();
-
 	// Add the employee to data array
 	this.data.push( newEmployee );
-
 	// Update the employee list
 	this.displayEmployees();
-
 }
-
-employees.resetForm = function () { // Resets the add employee form
-	// Loop through all form fields and set value to empty
+// Resets the add employee form
+employees.resetForm = function () { 
+	// Loop through provided form fields
 	for( var i = 0; i < this.fields.length; i++ ) {
+		// Set each field's value to empty
 		document.getElementById( this.fields[i] ).value = "";
+		// Focus on the ID field for better user experience
 		document.getElementById( this.fields[2] ).focus();
 	}
 	return true;
 }
-
-employees.displayEmployees = function () { // Generate and display all employees in the DOM
+// Generate and display all employees in the DOM
+employees.displayEmployees = function () { 
 	// Get the container element
 	var display = document.getElementById("dataDisplay");
-	display.innerHTML = employees.defaultMessage;
+	// Clear the display
+	display.innerHTML = "";
+	// If there are no employee objects present in the dataset
 	if( this.data.length < 1 ) {
-		// If the data.length is less than one ( no data exists ) show a default message
+		// Show the default message
 		display.innerHTML = '<td colspan="6" id="defaultDisplay">No Employees</td>';
 	} else {
-		// If there is data, loop through it and concat a string to display inside the container
+		// If there is data, loop through them
 		for ( var i = 0; i < this.data.length; i++ ) {
+			// Concat a string of HTML for each employee object and build a table row
 			display.innerHTML += '<tr>' +
 									'<td>' + this.data[i].employeeId + '</td>' +
 									'<td>' + this.data[i].firstName + '</td>' +
@@ -63,31 +67,43 @@ employees.displayEmployees = function () { // Generate and display all employees
 								'</tr>';
 		}
 	}
-	// Always update the monthly salary total
+	// Update the salary total each time - Pro Mode
 	this.displayTotalMonthlySalary();
 }
-
-employees.removeEmployee = function ( employeeIndex ) { // Remove an employee
-	var confirmation = window.confirm("Are you sure?"); // Confirm popup
+// Remove an employee
+// The function call built into HTML passes the employee's indexOf
+employees.removeEmployee = function ( employeeIndex ) {
+	// Ask user if they are sure they want to delete the employee
+	var confirmation = window.confirm("Are you sure?");
+	// If the user says confirms
 	if ( confirmation ) {
-		// If the user confirms
-		this.data.splice( employeeIndex, 1 ); // Remove the employee
+		// Remove the employee from the dataset array
+		this.data.splice( employeeIndex, 1 );
+		// Regenerate the display view
 		this.displayEmployees(); // Display the employees
 	} else {
 		return false;
 	}
 	
 }
-
-employees.displayTotalMonthlySalary = function () { // Places the Total Monthly Salary into the correct table cell
+// Places the Total Monthly Salary into the correct table cell
+employees.displayTotalMonthlySalary = function () {
+	// Find the salaryTotal cell
 	var cell = document.getElementById( "salaryTotal" );
+	// Replace its HTML with the totalMonthlySalary number
 	cell.innerHTML = this.getTotalMonthlySalary();
 }
-
-employees.getTotalMonthlySalary = function () { // Gets the Total Monthly Salary
+// Returns the Total Monthly Salary
+employees.getTotalMonthlySalary = function () {
+	// Set total to 0
 	var total = 0;
+	// Loop through each employee object in the dataset
 	for( var i = 0; i < this.data.length; i++ ) {
+		// Add the salaries to the total
 		total += parseInt( this.data[i].salary );
 	}
-	return ( total / 12 );
+	// Find the total Monthly Salary
+	total = ( total / 12 );
+	// Return the Total Monthly Salary
+	return total;
 }
